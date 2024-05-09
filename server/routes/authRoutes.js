0f4b0bed233723e87a2bbadc9d8e1cb9d59cf125
Madaure Thulani <calnganyi@gmail.com>
2024-05-09 12:21:16 +0200

    fixed server

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const CreateUsers = require("../controllers/users.controller");
 const Signup = require("../controllers/signup.auth");
 const Login = require("../controllers/auth.controller");
 const Logout = require("../controllers/logout");
-
 
 const router = express.Router();
 
@@ -11,11 +9,5 @@ router.post("/signup", Signup);
 router.post("/user/create", Signup);
 router.post("/login", Login);
 router.post("/logout", Logout);
-
-
-
-
-/*
-router.post("/logout", logout);*/
 
 module.exports = router;
